ac5820abd0bf0648e3fc3dcf4c5ec24e5dcb5724
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-08 13:40:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 //your JS code here. If required.
 
-// let increment = document.getElementById("incrementBtn");
-// let counter = document.getElementById("counter");
+let increment = document.getElementById("incrementBtn");
+let counter = document.getElementById("counter");
 
-// function callme() {
-//     alert(parseInt(counter.innerText)-1);   
-// }
+function callme() {
+    alert(parseInt(counter.innerText));   
+	counter.innerText = parseInt(counter.innerText) + 1;
+}
 
-// increment.addEventListener("click", callme);
+increment.addEventListener("click", callme);
 
- document.addEventListener('DOMContentLoaded', function() {
-            let counter = document.getElementById('counter');
-            let incrementBtn = document.getElementById('incrementBtn');
-
-            incrementBtn.addEventListener('click', function() {
-                alert('Current Counter Value: ' + counter.innerText);
-            });
-        });
