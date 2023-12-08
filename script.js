4d326dff50211a16a42e2db0455496f5b1f44b2a
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-08 12:56:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// let increment = getElementById("incrementBtn");
+// funtion callme(){
+// 	return alert("1");
+// }
+
+// increment.addEventListener("click",callme);
+
+let increment = document.getElementById("incrementBtn");
+let counter = document.getElementById("counter");
+
+function callme() {
+    alert(parseInt(counter.innerText)+1);
+}
+
+increment.addEventListener("click", callme);
